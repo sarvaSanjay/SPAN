4ace46a60d54a4462cfc9afd61cec185e87ec485
Reddy-Boss-420 <prashanth.shyamala@mail.utoronto.ca>
2023-01-21 17:08:30 -0500

    added distance functionality

---
+++ backend/map_func.py
@@ -0,0 +1,13 @@
+import math
+
+def isclose(curr_position, target_position):
+    R = 6378.137 #Radius of earth in KM
+    dLat = curr_position[0] * math.pi / 180 - target_position[0] * math.pi / 180
+    dLon = curr_position[1] * math.pi / 180 - target_position[1] * math.pi / 180
+    a = math.sin(dLat/2) * math.sin(dLat/2) + math.cos(target_position[0] * math.pi / 180) * math.cos(curr_position[0] * math.pi / 180) * math.sin(dLon/2) * math.sin(dLon/2)
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+    d = R * c
+    distance = d * 1000
+    return distance
+
+print(isclose((43.6426,79.3871), (43.6426,79.3872)))
